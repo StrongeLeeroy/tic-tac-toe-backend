9b31e7c374d2730428980bb4005d0e90decabc51
Gorka Hernandez <gorka.hernandez@datagenicgroup.com>
2016-04-01 17:12:35 +0200

    Change default port.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 require('babel-register');
 
 const server = require('./server/server.jsx').default;
-const PORT = process.env.PROD || 8083;
+const PORT = process.env.port || 1337;
 
 server.listen(PORT, function() {
     console.log('Server listening on port', PORT);
